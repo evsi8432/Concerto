ab48f98223122cafbb9bdbb7282e2f522be51825
IanChar <ianchar4@gmail.com>
2018-02-24 15:44:33 -0700

    Added extensions in config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import AppBar from 'material-ui/AppBar';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+import NavBar from './NavBar';
 
 export default class App extends React.Component {
     render() {
         return (
           <div>
-          <MuiThemeProvider>
-            <AppBar
-              title="Concerto"
-            />
-          </MuiThemeProvider>
+            <NavBar />
             <div style={{textAlign: 'center'}}>
                 <h1>Hello World</h1>
             </div>
