38457df9f4ceda81587924498139bbf664b1571e
Evan Sidrow <evsi8432@colorado.edu>
2018-02-24 15:50:09 -0700

    Added button stuffs

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import NewTrackButton from './NewTrackButton';
 
 export default class App extends React.Component {
     render() {
         return (
             <div style={{textAlign: 'center'}}>
+                <NewTrackButton />
                 <h1>Hello World</h1>
             </div>);
     }
