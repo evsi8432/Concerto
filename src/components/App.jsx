fef367935460954c38fbae57481cf30a93d160af
IanChar <ianchar4@gmail.com>
2018-02-24 15:20:43 -0700

    Added navbar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends React.Component {
     render() {
         return (
+          <div>
+          <MuiThemeProvider>
+            <AppBar
+              title="Concerto"
+            />
+          </MuiThemeProvider>
             <div style={{textAlign: 'center'}}>
                 <h1>Hello World</h1>
-            </div>);
+            </div>
+          </div>);
     }
 }
