d60063693da362ea1261c348c9816feac7dcb588
IanChar <ianchar4@gmail.com>
2018-02-24 17:12:39 -0700

    Have radio click function, doesnt change to filled

--- src/components/TrackColumn.jsx
+++ src/components/TrackColumn.jsx
@@ -16,29 +16,38 @@ const styles = {
 export default class TrackColumn extends React.Component {
     constructor(props) {
       super(props);
+      this.state = {
+        isOn: ['not_light', 'not_light', 'not_light'],
+      };
     }
 
     componentDidMount() {
       // Create listener for track id found in props.
     }
 
+    radioClick(radioNum) {
+      return (radioNum) => {
+        console.log(this.state.isOn[radioNum]);
+        if (this.state.isOn[radioNum] == 'light') {
+          this.state.isOn[radioNum] = 'not_light';
+        } else {
+          this.state.isOn[radioNum] = 'light';
+        }
+      }
+    }
+
     render() {
+      const radios = [];
+      for (var i = 0; i < this.state.isOn.length; i++) {
+        radios.push(<RadioButton
+                        value={this.state.isOn[i]}
+                        style={styles.radioButton}
+                        onClick={this.radioClick(i)}
+                      />)
+      }
         return (
           <MuiThemeProvider>
-            <RadioButtonGroup name="shipSpeed" defaultSelected="not_light">
-              <RadioButton
-                value="light"
-                style={styles.radioButton}
-              />
-              <RadioButton
-                value="light"
-                style={styles.radioButton}
-              />
-              <RadioButton
-                value="light"
-                style={styles.radioButton}
-              />
-            </RadioButtonGroup>
+            { radios }
           </MuiThemeProvider>);
     }
 }
